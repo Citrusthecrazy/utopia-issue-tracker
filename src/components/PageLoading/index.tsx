6da971e013c72af820889f671ba197f3ccbd7cc3
Marko <drakulic.marko0@gmail.com>
2022-08-01 00:49:34 +0200

    Updated page loading component

--- src/components/PageLoading/index.tsx
+++ src/components/PageLoading/index.tsx
@@ -4,9 +4,6 @@ const PageLoading = () => {
   return (
     <div className="bg-sky-900 h-screen flex flex-col items-center justify-center text-white">
       <h1 className="text-2xl">Ucitavanje...</h1>
-      <Link href="/" className="text-red-500">
-        Nazad na pocetnu
-      </Link>
     </div>
   );
 };
