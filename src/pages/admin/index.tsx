d9b4f70740b8532cd0979dcce17c4a770e8e4509
Marko <drakulic.marko0@gmail.com>
2022-08-01 00:55:50 +0200

    Added a page title on admin page

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,6 @@
 import { Issue } from "@prisma/client";
 import { useSession } from "next-auth/react";
+import Head from "next/head";
 import React, { FC } from "react";
 import { PageLoading, Unauthorized } from "../../components";
 import { trpc } from "../../utils/trpc";
@@ -27,23 +28,28 @@ const Admin = () => {
   }
 
   return (
-    <main className="relative bg-sky-900 h-screen text-white overflow-auto py-4">
-      <Header />
-      <div className="pt-16" />
-      {/* Header space */}
-      <div className="grid px-4 grid-cols-1 grid-rows-auto gap-y-8 md:gap-x-8 md:px-16 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
-        {issuesLoading && <div>Ucitavanje problema...</div>}
-        {issues &&
-          !issuesLoading &&
-          issues.map((issue: Issue) => (
-            <IssueCard
-              key={issue.id}
-              {...issue}
-              statusMutation={statusMutation}
-            />
-          ))}
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Admin Panel</title>
+      </Head>
+      <main className="relative bg-sky-900 h-screen text-white overflow-auto py-4">
+        <Header />
+        <div className="pt-16" />
+        {/* Header space */}
+        <div className="grid px-4 grid-cols-1 grid-rows-auto gap-y-8 md:gap-x-8 md:px-16 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+          {issuesLoading && <div>Ucitavanje problema...</div>}
+          {issues &&
+            !issuesLoading &&
+            issues.map((issue: Issue) => (
+              <IssueCard
+                key={issue.id}
+                {...issue}
+                statusMutation={statusMutation}
+              />
+            ))}
+        </div>
+      </main>
+    </>
   );
 };
 
