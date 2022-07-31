7adccbac6d5e4b692214acc791b1dc78e293ec6b
Marko <drakulic.marko0@gmail.com>
2022-08-01 00:57:55 +0200

    Added better loading state on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { Toaster } from "react-hot-toast";
-import { Login, QuestionForm } from "../components";
+import { Login, PageLoading, QuestionForm } from "../components";
 import { Header } from "../widgets";
 
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
+  if (status === "loading") {
+    return <PageLoading />;
+  }
   return (
     <>
       <Head>
@@ -20,7 +23,7 @@ const Home: NextPage = () => {
 
       <Toaster position="bottom-center" />
       <main className="relative bg-sky-900 h-screen w-screen text-white flex flex-col items-center justify-center overflow-x-hidden md:px-16">
-        {(session || status !== "loading") && <Header />}
+        {session && <Header />}
         <h1 className="text-[2rem] md:text-[5rem] xl:text-[6rem] font-bold mb-16">
           Utopia problemi
         </h1>
